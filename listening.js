c23521b596486cfaf25ffcf74a02f904772aba0a
underethsea <underethsea@gmail.com>
2024-07-08 21:36:56 +0000

    listening

--- listening.js
+++ listening.js
@@ -14,20 +14,23 @@ try {
 const CHAINNAME = getChainConfig().CHAINNAME;
 const CHAINID = getChainConfig().CHAINID;
 
-// const { CONTRACTS, ADDRESS, PROVIDERS } = require("./constants/index.js")
-const { ADDRESS } = require("./constants/address")
-const { WS_PROVIDERS, PROVIDERS } = require("./constants/providers")
-const { CONTRACTS } = require("./constants/contracts")
-const { TOPICS } = require("./constants/events")
-//const { PrizeWinsToDb } = require("./functions/prizeWinsToDb.js")
-//const LiquidateNow  = require("./liquidator.js")
-const { AddClaim } = require("./functions/dbDonkey.js")
-const { DiscordNotifyClaimPrize, SendMessageToChannel } = require("./functions/discordAlert.js")
-//const { DailyReport } = require("./functions/dailyReport")
-const chain = CHAINNAME
-const chainId = CHAINID
-const prizepool = ADDRESS[CHAINNAME].PRIZEPOOL
-const {FoundryPrizeWinsToDb} = require("./functions/foundryPrizeWinsToDb.js")
+const { ADDRESS } = require("./constants/address");
+const { WS_PROVIDERS, PROVIDERS } = require("./constants/providers");
+const { CONTRACTS } = require("./constants/contracts");
+const { TOPICS } = require("./constants/events");
+const { AddClaim } = require("./functions/dbDonkey.js");
+const { DiscordNotifyClaimPrize, SendMessageToChannel } = require("./functions/discordAlert.js");
+const { FoundryPrizeWinsToDb } = require("./functions/foundryPrizeWinsToDb.js");
+
+const chain = CHAINNAME;
+const chainId = CHAINID;
+const prizepool = ADDRESS[CHAINNAME].PRIZEPOOL;
+
+const fs = require('fs');
+const path = require('path');
+const lockFilePath = path.join(__dirname, 'calc.lock');
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 let LISTENPROVIDER;
 
@@ -39,98 +42,98 @@ if (WS_PROVIDERS[chain]) {
 }
 
 const FILTERS = {
-// draw awarded
   DRAWAWARDED: {
     address: ADDRESS[chain].PRIZEPOOL,
     topics: [TOPICS.DRAWAWARDED]
   },
-/*
-  DRAWCLOSED: {
-    address: ADDRESS[chain].PRIZEPOOL,
-    topics: ["0x330a72bcfcfc5c9a30ef8bdce22a7499068847d7e5ad7330f3a81fdd1e6e996d"]
-},
-// old for sepolia
-/*  CLAIMEDPRIZE: {
-    address: ADDRESS[chain].PRIZEPOOL,
-    topics: ["0xc31bc4fb7f1c35cfd7aa34780f09c3f0a97653a70920593b2284de94a4772957"]
-  },*/
- CLAIMEDPRIZE: {
+  CLAIMEDPRIZE: {
     address: ADDRESS[chain].PRIZEPOOL,
     topics: [TOPICS.CLAIMEDPRIZE]
   },
-}
+};
 
 async function listen() {
-    console.log("listening for complete award and claim events")
-     LISTENPROVIDER.on(FILTERS.DRAWAWARDED, (drawCompletedEvent) => {
-             console.log("draw completed event", drawCompletedEvent)
-		//try{DailyReport()}catch(e){console.log(e)}
+  console.log("listening for complete award and claim events");
 
-/*
-setTimeout(() => {
-  try{
-// alert to pooltime test channel
-await SendMessageToChannel("1225048554708406282","Draw awarded on "+chain)
-}catch(e){console.log("error sending msg to discord of draw event")}
-  try {
-    FoundryPrizeWinsToDb(chainId, drawCompletedEvent.blockNumber)
-      .then((finished) => {
-        console.log("db updated");
-      });
-  } catch (error) {
-    console.log(error);
-  }
-}, 90000);
-*/
-setTimeout(() => {
-            // Use an IIFE to handle async call
-            (async () => {
-                try {
-                    // alert to pooltime test channel
-                    await SendMessageToChannel("1225048554708406282", "Draw awarded on " + chain);
-                } catch (e) {
-                    console.log("error sending msg to discord of draw event", e);
-                }
+  LISTENPROVIDER.on(FILTERS.DRAWAWARDED, async (drawCompletedEvent) => {
+    console.log("draw completed event", drawCompletedEvent);
 
-                try {
-                    await FoundryPrizeWinsToDb(chainId, drawCompletedEvent.blockNumber);
-                    console.log("db updated");
-                } catch (error) {
-                    console.log(error);
-                }
-            })();
-        }, 90000);
-//try{LiquidateNow()}catch(e){console.log(e)}     
-          })
-//console.log(WS_PROVIDERS[chain])
-    LISTENPROVIDER.on(FILTERS.CLAIMEDPRIZE, (claimEvent) => {
-      try {
-          // console.log("prize claimed event ", claimEvent)
-          const decodedLog =
-        CONTRACTS.PRIZEPOOL[chain].interface.parseLog(claimEvent);
-//console.log("claim event",decodedLog)        
-const args = decodedLog.args
-        const claim = {
-          network: chainId,
-          drawId: args.drawId,
-          vault: args.vault.toLowerCase(),
-          winner: args.winner.toLowerCase(),
-          tier: args.tier,
-          index: args.prizeIndex,
-          payout: args.payout,
-          fee: args.claimReward,
-          miner: args.claimRewardRecipient.toLowerCase(),
-          hash: claimEvent.transactionHash.toLowerCase(),
-          block: claimEvent.blockNumber,
-          chainName: chain
-        };
-        AddClaim(chainId,prizepool,claim).then((finished)=>{})
-        
-       if(claim.payout.gt(0)){DiscordNotifyClaimPrize(claim,prizepool).then((finished)=>{})}
+    try {
+      await SendMessageToChannel("1225048554708406282", "Draw awarded on " + chain);
+    } catch (e) {
+      console.log("Error sending msg to Discord of draw event:", e);
+    }
 
-      }catch(error){console.log(error)}
-  })
+    setTimeout(async () => {
+      await startCalculation(chainId, drawCompletedEvent);
+    }, 60000);
+  });
+
+  LISTENPROVIDER.on(FILTERS.CLAIMEDPRIZE, (claimEvent) => {
+    try {
+      const decodedLog = CONTRACTS.PRIZEPOOL[chain].interface.parseLog(claimEvent);
+      const args = decodedLog.args;
+      const claim = {
+        network: chainId,
+        drawId: args.drawId,
+        vault: args.vault.toLowerCase(),
+        winner: args.winner.toLowerCase(),
+        tier: args.tier,
+        index: args.prizeIndex,
+        payout: args.payout,
+        fee: args.claimReward,
+        miner: args.claimRewardRecipient.toLowerCase(),
+        hash: claimEvent.transactionHash.toLowerCase(),
+        block: claimEvent.blockNumber,
+        chainName: chain
+      };
+      AddClaim(chainId, prizepool, claim).then((finished) => {});
+
+      if (claim.payout.gt(0)) {
+        DiscordNotifyClaimPrize(claim, prizepool).then((finished) => {});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 
-listen()
+async function startCalculation(chainId, drawCompletedEvent) {
+  const maxRetries = 60;
+  const retryDelay = 60000;
+  const lockTimeout = 30 * 60 * 1000;
 
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    if (fs.existsSync(lockFilePath)) {
+      const lockTime = fs.readFileSync(lockFilePath, 'utf8');
+      const lockDate = new Date(parseInt(lockTime, 10));
+      const currentDate = new Date();
+
+      if (currentDate - lockDate > lockTimeout) {
+        console.log('Lock file is older than 30 minutes. Proceeding with calculation.');
+      } else {
+        console.log(`Calculation in progress. Attempt ${attempt} failed. Retrying in 1 minute...`);
+        await delay(retryDelay);
+        continue;
+      }
+    }
+
+    fs.writeFileSync(lockFilePath, Date.now().toString());
+
+    try {
+      console.log('Starting FoundryPrizeWinsToDb calculation...');
+      await FoundryPrizeWinsToDb(chainId, drawCompletedEvent.blockNumber);
+      console.log("DB updated.");
+      fs.unlinkSync(lockFilePath);
+      return;
+    } catch (error) {
+      console.error('Error during calculation:', error);
+      fs.unlinkSync(lockFilePath);
+      return;
+    }
+  }
+
+  console.log('Max retries reached. Calculation did not start.');
+}
+
+listen();
