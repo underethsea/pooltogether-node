d522e2241855aa3b1d4f193f2f6c95b405118057
underethsea <underethsea@gmail.com>
2025-02-12 22:27:23 +0000

    chain specific config

--- constants/config.example.js
+++ constants/config.example.js
@@ -5,23 +5,24 @@ const CONFIG = {
   CHAINID: 10,
   WALLET: process.env.WALLET, // Signing wallet address
 
-  PRIORITYFEE: ".001", // priority fee for sending transactions ".001" is .001 gwei
+  USEEXPLORERPRICING: true,
+  PRIORITYFEE: ".00105", // priority fee for sending transactions ".001" is .001 gwei
   // Prize Calculator Configuration
   BATCHSIZE: 100, // Number of wins to calculate per multicall
   useCoinGecko: true, // Use CoinGecko for pricing (false uses 1inch)
   // Timing Configuration for looping claimer and liquidator apps
   // retries will be a random time between min and max
-  minTimeInMilliseconds: 300000, // Minimum polling interval (300000 = 5 minutes)
-  maxTimeInMilliseconds: 600000, // Maximum polling interval 
+  minTimeInMilliseconds: 180 * 1000, // Minimum polling interval (180* 1000 = 3 minutes)
+  maxTimeInMilliseconds: 300 * 1000, // Maximum polling interval (300* 1000 = 5 minutes)
 
   // Claimer Configuration
   USEAPI: "pooltime", // "none" or "pooltime" or "g9"
   TIERSTOCLAIM: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   TXDELAY: 10000, // Delay between transactions in milliseconds
-  MINPROFIT: 0.01, // Minimum acceptable profit in $ for a claim transaction
+  MINPROFIT: 0.05, // Minimum acceptable profit in $ for a claim transaction
   MINPROFITPERCENTAGE: 1.001, // Minimum profit percentage for acceptance, 1.01 = 101%
-  MAXWINNERS: 14, // Maximum number of winners to claim per transaction
-  MAXINDICES: 16, // Maximum number of prize indices to claim per transaction
+  MAXWINNERS: 10, // Maximum number of winners to claim per transaction
+  MAXINDICES: 15, // Maximum number of prize indices to claim per transaction
   //  USESANTA: false, // Use contract for claims with contract to sell claim fees back to WETH
   REWARDS_CLAIM_WAIT: 8, // hours to wait to check if rewards are claimable (reduces RPC pings)
   MINTOCLAIM: 0.005, // the claimer will collect rewards once the minimum is reached (.1 = .1 or more prize tokens)
@@ -29,14 +30,18 @@ const CONFIG = {
     // these addresses are pushed to back of claim list (deprioritized)
   ],
   // Liquidator Configuration
-  slippage: 5, // Slippage tolerance for transactions (basis points)
-  profitThreshold: 0.001, // Profit threshold in $ for considering a transaction
+  slippage: 0, // Slippage tolerance for transactions (basis points)
+  profitThreshold: 0.10, // Profit threshold in $ for considering a transaction
   profitPercentage: 1.0001, // Required profit percentage over cost
   //  ONLYLIQUIDATE: ['0x7d72e1043FBaCF54aDc0610EA8649b23055462f0','0x006e714accBFEecD561a9B590e919402e871a91D'], // array of pairs to liquidate
-  MIN_LIQUIDATE: 0.05, // min to liquidate
+  MIN_LIQUIDATE: 0.10, // min to liquidate
   ONLYLIQUIDATE: [],
   DONTLIQUIDATE: [], // array of pairs to exclude
-  SWAPPERS: {}, // {CHAINNAME: "contract address",etc}
+SWAPPERS: {BASE:"0xf152cec8b04695705772012764d2abe2e5cbfd38",
+OPTIMISM:"0xf152cec8b04695705772012764d2abe2e5cbfd38",
+ARBITRUM:"0xf152CEC8B04695705772012764D2ABE2e5cbfd38",
+//ETHEREUM:"0xf152CEC8B04695705772012764D2ABE2e5cbfd38"
+},
 
   // WinBooster Configuration
   FEE: 4, // claimer fee (ex 5 = 5%)
@@ -56,9 +61,29 @@ const CONFIG = {
   // DrawAuction Config
   RNGRETRY: 80, // seconds to retry
 };
+
 Object.freeze(CONFIG);
 
-module.exports = { CONFIG };
+// USE TO OVERRIDE DEFAULT CONFIG
+const CHAIN_CONFIG = {
+BASE: { 
+ minTimeInMilliseconds: 60 * 1000, // Minimum polling interval (60 * 1000 = 1 minutes)
+ maxTimeInMilliseconds: 120 * 1000, // Maximum polling interval (120 * 1000 = 2 minutes)
+ MIN_LIQUIDATE: 0.05,
+ profitThreshold: 0.10, // Profit threshold in $ for considering a transaction
+ profitPercentage: 0.0002, // Required profit percentage over cost
+ },
+},
+}
+
+// Dynamic configuration function
+function Config(chainName) {
+  const chainKey = (chainName || CONFIG.CHAINNAME).toUpperCase();
+  const chainOverrides = CHAIN_CONFIG[chainKey] || {};
+  return { ...CONFIG, ...chainOverrides };
+}
+
+module.exports = { Config,CONFIG };
 
 // Additional network and wallet configurations
 // MUMBAI Testnet CHAINID: 80001
